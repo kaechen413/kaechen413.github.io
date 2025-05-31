b2b469f07ccadda22c5321df7d6f3d0d032ca6ab
Jiahui Chen <cjh1171740108@gmail.com>
2025-05-31 17:01:18 +0900

    backup

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,10 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.navbar-container .menu');
-const menuIcon = document.querySelector('.menu-btn i');
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('.menu-btn');
+  const menu = document.querySelector('.navbar-container .menu');
+  const menuLinks = document.querySelectorAll('.navbar-container .menu li a');
 
-menuBtn.addEventListener('click', function() {
-  menu.classList.toggle('active');
-  menuIcon.classList.toggle('active');
-});
+  menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      // Only close menu if it's open (mobile)
+      if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        menuBtn.classList.remove('active');
+      }
+    });
+  });
 });
