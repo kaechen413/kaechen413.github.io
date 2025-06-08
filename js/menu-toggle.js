7f581c7395cdf5c60ef4be224bf2b71861ae7141
Jiahui Chen <cjh1171740108@gmail.com>
2025-06-08 16:04:07 +0900

    finished the page

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.querySelector('.menu-btn');
+  const menuIcon = document.querySelector('.menu-btn i');
   const menu = document.querySelector('.navbar-container .menu');
   const menuLinks = document.querySelectorAll('.navbar-container .menu li a');
 
   menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     menuBtn.classList.toggle('active');
+        // Toggle icon
+    if (menuBtn.classList.contains('active')) {
+      menuIcon.classList.remove('fa-bars');
+      menuIcon.classList.add('fa-times');
+    } else {
+      menuIcon.classList.remove('fa-times');
+      menuIcon.classList.add('fa-bars');
+    }
   });
 
   menuLinks.forEach(link => {
@@ -14,6 +23,8 @@ document.addEventListener('DOMContentLoaded', () => {
       if (menu.classList.contains('active')) {
         menu.classList.remove('active');
         menuBtn.classList.remove('active');
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
       }
     });
   });
